c184b03ba125a0ad6c64c4175afa56396352849b
zhuy-nguyenz <graio911@gmail.com>
2021-06-28 09:32:22 +0700

    1st

---
+++ src/Ash/CrawlSong.java
@@ -0,0 +1,28 @@
+package Ash;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CrawlSong {
+    public static void main(String[] args) {
+        try{
+            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            Scanner input = new Scanner(new InputStreamReader(url.openStream()));
+            input.useDelimiter("\\Z");
+            String content = input.next();
+            input.close();
+            content = content.replaceAll("\\n+","");
+            Pattern pattern = Pattern.compile("name_song\">(.*?)</a>");
+            Matcher matcher = pattern.matcher(content);
+            while(matcher.find()){
+                System.out.println(matcher.group(1));
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
